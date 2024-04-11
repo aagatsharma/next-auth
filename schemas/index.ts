f481bda26d448ccf226b40ef471c859d10d95558
aagatsharma <aagats72@gmail.com>
2024-04-11 15:11:17 +0545

    reset password form

--- schemas/index.ts
+++ schemas/index.ts
@@ -17,3 +17,7 @@ export const RegisterSchema = z.object({
 export const ResetSchema = z.object({
   email: z.string().email({ message: "Email is required" }),
 });
+
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6, { message: "Minimum 6 character required" }),
+});
