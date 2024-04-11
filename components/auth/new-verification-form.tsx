1be3e9b87faa3acb210623920246a1a10611e256
aagatsharma <aagats72@gmail.com>
2024-04-11 10:19:24 +0545

    email verification

---
+++ components/auth/new-verification-form.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import CardWrapper from "./card-wrapper";
+import { useCallback, useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { newVerification } from "@/actions/new-verification";
+import { Loader2 } from "lucide-react";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+
+const NewVerificationForm = () => {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+
+  const onSubmit = useCallback(() => {
+    if (!token) {
+      setError("Missing Token");
+      return;
+    }
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.success);
+        setError(data.error);
+      })
+      .catch(() => {
+        setError("Something went wrong");
+      });
+  }, [token]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+  return (
+    <CardWrapper
+      headerLabel="Confirming your verification"
+      backButtonLabel="Back to Login"
+      backButtonHref="/auth/login"
+    >
+      <div className="flex items-center justify-center w-full">
+        {!success && !error && <Loader2 className="animate-spin" />}
+        <FormSuccess message={success} />
+        {!success && <FormError message={error} />}
+      </div>
+    </CardWrapper>
+  );
+};
+
+export default NewVerificationForm;
